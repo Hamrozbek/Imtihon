8ea1f8d9cf0b9db181e72f98e7f65852f489cca8
Hamrozbek <hamroznuriddinov41@gmail.com>
2025-08-11 10:46:21 +0500

    start pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
-        {/* <BrowserRouter> */}
-            <CookiesProvider>
-                <App />
-            </CookiesProvider>
-    {/* </BrowserRouter> */}
-    </HashRouter>
+    <BrowserRouter>
+        <CookiesProvider>
+            <App />
+        </CookiesProvider>
+    </BrowserRouter>
 )
