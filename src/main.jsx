6ab02a3eac7656fa1eeb4a3955e09d9392e48534
Hamrozbek <hamroznuriddinov41@gmail.com>
2025-08-11 10:42:54 +0500

    snana

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <CookiesProvider>
-            <App />
-        </CookiesProvider>
-    </BrowserRouter>
+    <HashRouter>
+        {/* <BrowserRouter> */}
+            <CookiesProvider>
+                <App />
+            </CookiesProvider>
+    {/* </BrowserRouter> */}
+    </HashRouter>
 )
